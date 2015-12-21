6741df5ec1d52a020293c07a4840b158a8e4e346
M.C. van den Boogaart <scola@kvdb.name>
2015-12-21 23:09:27 +0100

    Initial import

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const DI = require('@scola/di');
+const MVA = require('@scola/mva');
+
+class Module extends DI.Module {
+  configure() {
+    this.addModule(MVA.Module);
+
+    const definition = {
+      'form': {
+        'button-icon': require('./lib/form/button-icon'),
+        'button-text': require('./lib/form/button-text'),
+        'checkbox': require('./lib/form/checkbox'),
+        'checkbox-label': require('./lib/form/checkbox-label'),
+        'form': require('./lib/form/form'),
+        'input': require('./lib/form/input')
+      },
+      'widget': {
+        'card': require('./lib/widget/card')
+      }
+    };
+
+    const views = {};
+
+    Object.keys(definition).forEach((group) => {
+      Object.keys(definition[group]).forEach((name) => {
+        views['@scola.' + group + '.' + name] =
+          this.instance(definition[group][name]);
+      });
+    });
+
+    const styles = {
+      'absmax': {
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+        right: 0,
+        top: 0
+      }
+    };
+
+    this.inject(MVA.View.Dispatcher)
+      .assignArgument(0, this.object(views));
+
+    this.inject(MVA.View.Dispatcher)
+      .assignArgument(2, this.object(styles));
+  }
+}
+
+module.exports = {
+  Module
+};
